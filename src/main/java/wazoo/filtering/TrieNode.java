af2e5781a4a7ee0551efd7a1876004e777abd6f5
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-07-27 01:17:12 +0900

    [feat] 비속어 필터링 기능 구현

---
+++ src/main/java/wazoo/filtering/TrieNode.java
@@ -0,0 +1,9 @@
+package wazoo.filtering;
+
+import java.util.*;
+
+public class TrieNode {
+    Map<Character, TrieNode> children = new HashMap<>();
+    TrieNode fail;
+    Set<String> outputs = new HashSet<>();
+}
